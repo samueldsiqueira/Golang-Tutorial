4a31d98848dd51da8e23921b87f99095d1bcf14a
Samuel D Siqueira <samueldsiqueira@gmail.com>
2024-03-18 14:01:48 -0300

    Aula-1: Hello World

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+var lastName = "D' Siqueira"
+
+func main() {
+	 language, name := "go", "Samuel"
+
+	fmt.Println("my name is", name, lastName)
+	fmt.Println("and I am learning", language)
+}
