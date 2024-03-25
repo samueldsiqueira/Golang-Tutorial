b44a645035b632de7eec433b18e27080a3a5d05f
Samuel D' Siqueira <samueldsiqueira@gmail.com>
2024-03-25 14:59:14 -0300

    aula sobre computadores

---
+++ 07-como-os-computadores-funcionam/main.go
@@ -0,0 +1,7 @@
+package main
+
+// nessa aula é tudo mais ligado a explicação de linguagem de máquina ASCII
+// - Isso é importante pois daqui pra frente vamos falar de ints, bytes, e etc.
+// - Não é necessário um conhecimento a fundo mas é importante ter uma idéia de como as coisas funcionam por trás dos panos.
+// - https://docs.google.com/presentation/...
+// - ASCII: https://en.wikipedia.org/wiki/ASCII
